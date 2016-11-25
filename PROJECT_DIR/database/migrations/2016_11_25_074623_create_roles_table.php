a6dc561f64a62639fc5bc1b9a313f57c2f9318c4
gig <grig@esso.ru>
2016-11-25 11:42:08 +0300

    3. Сделаны миграции - заполнение таблиц

--- PROJECT_DIR/database/migrations/2016_11_25_074623_create_roles_table.php
+++ PROJECT_DIR/database/migrations/2016_11_25_074623_create_roles_table.php
@@ -13,7 +13,7 @@ class CreateRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('role', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('role');
             $table->rememberToken();
---
+++ PROJECT_DIR/database/seeds/RolesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'role' => 'Админ',
+        ]);
+
+        DB::table('roles')->insert([
+            'role' => 'Пользователь',
+        ]);
+
+
+    }
+}
