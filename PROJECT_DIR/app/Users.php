6c695ff8b90c299f704637442ebcd7d160da6623
gig <grig@esso.ru>
2016-11-29 12:18:48 +0300

    Написан метод по пункту 1.

--- PROJECT_DIR/app/Users.php
+++ PROJECT_DIR/app/Users.php
@@ -9,9 +9,33 @@ class Users extends Model
     protected $table = 'users';
     protected $fillable = ['name', 'patronymic', 'family', 'email', 'roles_id'];
 
+
     public function roles()
     {
         return $this->belongsTo('App\Roles', 'roles_id');
+    }
+
+    public function detail()
+    {
+
+        if (!empty($this->name) && !empty($this->patronymic) && !empty($this->family)) {
+
+            return $this->name . ' ' . $this->patronymic . ' ' . $this->family;
+
+        } elseif (!empty($this->name) && !empty($this->family)) {
+
+            return $this->name . ' ' . $this->family;
+
+        } elseif (empty($this->name) || empty($this->family)) {
+
+            return $this->email;
+
+        } else {
+
+            return false;
+
+        }
+
     }
 
 }
