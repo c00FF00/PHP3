adf5bf434e9b1d4c1dc074dde690920b28f65e58
gig <grig@esso.ru>
2016-12-12 12:40:23 +0300

    L05. Punkt 2. Сделан.

--- PROJECT_DIR/app/Users.php
+++ PROJECT_DIR/app/Users.php
@@ -8,6 +8,9 @@ class Users extends Model
 {
     protected $table = 'users';
     protected $fillable = ['name', 'patronymic', 'family', 'email', 'roles_id'];
+    protected $comment = 'template';
+    protected $firstName = 'Имя';
+    protected $lastName = 'Фамилия';
 
 
     public function roles()
@@ -15,13 +18,11 @@ class Users extends Model
         return $this->belongsTo('App\Roles', 'roles_id');
     }
 
-
-
     /**
      * @template "$firstName $lastName"
      *
      */
-    public static function getFullName($name, $patronymic, $family, $email)
+    public static function detail($name, $patronymic, $family, $email)
     {
 
         if (!empty($name) && !empty($patronymic) && !empty($family)) {
@@ -44,4 +45,48 @@ class Users extends Model
 
     }
 
+    /**
+     * @template "$firstName $lastName"
+     *
+     */
+    public function getFullName()
+    {
+
+        $name = $this->getTagComment($this->comment);
+
+        $name1 = str_replace('$', '', $name[1]);
+        $name2 = str_replace('$', '', $name[2]);
+
+        return  $this->$name1 . ' ' . $this->$name2;
+
+    }
+
+
+
+    public function getTagComment($commentkey)
+    {
+        $reflect = new \ReflectionMethod('App\Users', 'getFullName');
+        $this->comment = $reflect->getDocComment();
+
+        $pattern = '@' . $commentkey . '.*';
+
+        preg_match('/' . $pattern . '/', $this->comment, $result);
+        preg_replace('/\s+/', ' ', $result[0]);
+
+        $tmp = str_replace('"', '', $result[0]);
+
+        $element = explode(' ', $tmp);
+
+        unset($element[0]);
+
+        return $element;
+
+    }
+
+    
 }
+
+
+
+
+
