3d77f21a71c038dd08b2cd5a9a26030010145fa4
gig <grig@esso.ru>
2016-12-13 20:38:53 +0300

    Except CSRF for REST

--- PROJECT_DIR/app/Http/Controllers/DefaultController.php
+++ PROJECT_DIR/app/Http/Controllers/DefaultController.php
@@ -8,16 +8,35 @@ use Illuminate\Http\Request;
 
 class DefaultController extends Controller
 {
+
     public function index()
     {
         $domain = config('app.domain');
         return view('index')->with(compact('domain'));
     }
 
-    public function service()
+    public function server()
     {
-        $res = file_get_contents('php://input');
-        var_dump($res);
+        $data = json_decode(file_get_contents('php://input'));
+        var_dump($data); echo $data->fooo;
+//        $data->fooo = $data->fooo;
+    }
+
+    public function client()
+    {
+        $url = 'http://localhost/server';
+        $data = ['fooo' => 42, 'dddd' => 'Опоздал'];
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_HEADER, ['Content-type: application/json']);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($curl);
+        echo $result;
+        var_dump($result);
+        die;
+
+
     }
 
 
