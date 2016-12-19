90e565e0fc8cd70ff0545e3cda964778865cad5d
gig <grig@esso.ru>
2016-12-19 12:23:04 +0300

    Исправлена ошибка

--- PROJECT_DIR/tests/mytesting/UsersTestNF.php
+++ PROJECT_DIR/tests/mytesting/UsersTestNF.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class UsersTestNPF extends TestCase
+class UsersTestNF extends TestCase
 {
 
     public function testNamePatrFamily()
