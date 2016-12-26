4671d016ca45a80c715f14f7dd3b3e971592c4bd
gig <grig@esso.ru>
2016-12-26 11:08:36 +0300

    Полностью рабочая серверная часть микросервиса.
    Работоспособный клиент.
    
    Задача - логика для клиента. Отправка сообщений. Сделать шаблон.

--- RESTful/Classes/mailToBase.php
+++ RESTful/Classes/mailToBase.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
- * Class mailToBase
- */
 class mailToBase
 {
 
--- RESTful/microserviceClient.php
+++ RESTful/microserviceClient.php
@@ -1,4 +1,5 @@
 <?php
+
 require (__DIR__ . '/Classes/mailToBase.php');
 
 $request = new mailToBase('localhost', '8000');
@@ -6,7 +7,10 @@ $request = new mailToBase('localhost', '8000');
 $res = $request->setMethod('get')->sendRequest('message/status/expect');
 
 
+foreach ($res->answer as $obj) {
+    echo $obj->status; echo PHP_EOL;
+}
 
 
 
-print_r($res);
+//print_r($res);
