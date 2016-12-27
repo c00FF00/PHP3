8825f85403b638f32ac57fbf80c0292abaafd6f9
gig <grig@esso.ru>
2016-12-27 14:59:31 +0300

    Реестр начат.

--- L08/REST_Reestr/clientReestr.php
+++ L08/REST_Reestr/clientReestr.php
@@ -1,5 +1,18 @@
 <?php
 
 
+$curl = curl_init('https://www.nalog.ru/opendata/7707329152-registerdisqualified/data-25122016-structure-24062015.csv');
 
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, strtoupper('get'));
 
+//curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
+//
+//curl_setopt($curl, CURLOPT_POSTFIELDS, $this->getJsonData());
+
+//curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl);
+
+$reestr = explode(PHP_EOL, $result);
+
+var_dump($reestr);
