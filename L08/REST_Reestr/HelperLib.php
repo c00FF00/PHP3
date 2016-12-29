fc4f31108526e54ee032ede6e2fe9909a7d4eb5c
grig <grigory.e@gmail.com>
2016-12-29 22:41:32 +0300

    Fix client & server.

--- L08/REST_Reestr/HelperLib.php
+++ L08/REST_Reestr/HelperLib.php
@@ -9,10 +9,8 @@ function makeRegisterdisqualifiedURL($urlpage, $pattern)
 {
 
     $content = file_get_contents($urlpage);
-
-    $pattern = '/data-[0-9]{8}-structure-[0-9]{8}.csv/';
-
-    if (preg_match($pattern, $content, $match)) {
+    
+    if (preg_match('/' . $pattern . '/', $content, $match)) {
 
         return $urlpage . '/' . $match[0];
 
